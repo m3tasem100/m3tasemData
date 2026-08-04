40280a198ba66a9b2f0bbeed6c202d4241ed58fa
m3tasem100 <barahmeh@gmail.com>
2026-08-05 01:11:56 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,36 +1,95 @@
-// firebase-config.js
+////////////////////////////////////////////////////
+// FIREBASE CONFIGURATION
+// UCA SCHOOL MANAGEMENT SYSTEM
+////////////////////////////////////////////////////
 
 
-import { initializeApp } 
+
+import {
+
+initializeApp
+
+}
+
 from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 
 
-import { 
-getAuth 
+
+
+
+import {
+
+getAuth
+
 }
+
 from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 
-import { 
-getDatabase 
+
+
+
+import {
+
+getDatabase
+
 }
+
 from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 
 
 
 
+
+
+
+
+// ضع بيانات مشروعك من Firebase Console
+
 const firebaseConfig = {
 
 
- apiKey: "AIzaSyBWpahddzrX3iffUThUQoo5edxGbi0J7yc",
-  authDomain: "uca-pms-8f7b1.firebaseapp.com",
-  databaseURL: "https://uca-pms-8f7b1-default-rtdb.firebaseio.com",
-  projectId: "uca-pms-8f7b1",
-  storageBucket: "uca-pms-8f7b1.firebasestorage.app",
-  messagingSenderId: "559264692532",
-  appId: "1:559264692532:web:14426f15f058db05458d61",
-  measurementId: "G-EDMZHQRGHK"
+apiKey:
+
+"ضع_apiKey_هنا",
+
+
+
+authDomain:
+
+"ضع_authDomain_هنا",
+
+
+
+databaseURL:
+
+"https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+
+
+
+projectId:
+
+"YOUR_PROJECT_ID",
+
+
+
+storageBucket:
+
+"YOUR_PROJECT_ID.appspot.com",
+
+
+
+messagingSenderId:
+
+"ضع_sender_id_هنا",
+
+
+
+appId:
+
+"ضع_app_id_هنا"
+
 
 
 };
@@ -41,17 +100,49 @@ const firebaseConfig = {
 
 
 
-export const app = 
-initializeApp(firebaseConfig);
 
 
+// تشغيل Firebase
+
+
+const app =
+
+initializeApp(
+
+firebaseConfig
+
+);
+
+
+
+
+
+
+
+// Authentication
 
 
 export const auth =
-getAuth(app);
+
+getAuth(
+
+app
+
+);
 
 
+
+
+
+
+
+// Realtime Database
 
 
 export const db =
-getDatabase(app);
+
+getDatabase(
+
+app
+
+);
